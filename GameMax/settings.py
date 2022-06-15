7de9968cd9e0fa71be35fb7a27d8601c1ca3da36
SkAndres <andre.beltukov5656@gmail.com>
2022-06-15 23:30:44 +0300

    bug fix

--- GameMax/settings.py
+++ GameMax/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-q^n(&cjic%z9h*e48f3#v)=milv8murwp+cwxhc(v%4na_0)jl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
