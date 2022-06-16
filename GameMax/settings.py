c2584336b4ec3231d8ad83c6b62f692c34827254
SkAndres <andre.beltukov5656@gmail.com>
2022-06-16 11:14:09 +0300

    first commit

--- GameMax/settings.py
+++ GameMax/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import django_heroku
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^n(&cjic%z9h*e48f3#v)=milv8murwp+cwxhc(v%4na_0)jl'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -166,5 +166,3 @@ AWS_LOCATION_ADMIN = 'admin'
 ADMIN_MEDIA_PREFIX = "S3://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_ADMIN)
 DEFAULT_FILE_STORAGE = 'GameMax.storages.MediaStore'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
-
