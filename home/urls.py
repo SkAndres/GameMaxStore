c74a3d21c4b8455e7b0fa23a4a61535230ca77f2
SkAndres <andre.beltukov5656@gmail.com>
2022-06-18 08:23:37 +0300

    new commit (upgrade added categories for products)

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='home'),
     path('product/<str:item_id>', views.product,  name='product'),
-
     path('related_prod/<str:item_id>', views.product,  name='related_prod'),
     path('chosen_category/<str:new_category_id>', views.chosen_category, name='chosen_category'),
     path('new_order/<str:devices_id>', views.new_order, name='new_order'),
+    path('category/<str:category_id>', views.chosen_category, name='chosen_category')
 ]
