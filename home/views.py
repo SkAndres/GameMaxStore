145c608054b12483d4c46226b616e0e612d4f543
SkAndres <andre.beltukov5656@gmail.com>
2022-06-18 08:23:37 +0300

    new commit (upgrade added categories for products)

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from .models import Product, Order
 
 
 def index(request):
-    items = Product.objects.all()
+    items = Product.objects.all().order_by('-title')
     return render(request, 'home.html', {'items': items})
 
 
@@ -15,12 +15,13 @@ def product(request, item_id):
     return render(request, 'product.html', {'devices': devices, 'rel_prods': rel_prods})
 
 
-def chosen_category(request, new_category_id):
-    by_category = Product.objects.get(id=new_category_id)
+def chosen_category(request, category_id):
+    by_category = Product.objects.filter(category=category_id)
+    print(by_category)
     return render(request, 'category.html', {'by_category': by_category})
 
 
 def new_order(request, devices_id):
-    products = Product.objects.get(id=devices_id)
+    products = Product.objects.get(pk=devices_id)
     order = Order(request)
     order.add(product=products)
