126cd824a5a64994d5c289e205ad805a7d1bba79
SkAndres <andre.beltukov5656@gmail.com>
2022-07-17 15:25:19 +0300

    bug fix (fixed bug with rel_prod)

--- GameMax/settings.py
+++ GameMax/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG = int(os.environ.get('DEBUG', default=1))
 
 ALLOWED_HOSTS = ['*']
